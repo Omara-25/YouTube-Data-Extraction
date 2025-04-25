bdb528a0ba93b5878bb72f2c15bf1651608c1f01
Mohamed omara <omara@criticalfuture.co.uk>
2025-04-25 04:20:12 +0300

    solving issues

---
+++ config.js
@@ -0,0 +1,7 @@
+// Configuration file for YouTube Data Extractor
+// DO NOT commit this file to version control
+
+// YouTube API Key
+const config = {
+  YOUTUBE_API_KEY: 'AIzaSyAP0QxoQh7i7ELEKZRApY2S0FHSL8MqsNM'
+};
